2081a6d8fc46363b834213aa11037c90d2bde186
Tom Howard <thomashoward@deloitte.co.uk>
2021-01-26 10:51:19 +0000

    Initial commit

---
+++ docs/cmdline-opts/aws-sigv4.d
@@ -0,0 +1,10 @@
+Long: aws-sigv4
+Arg: <provider1[:provider2]>
+Help: Use AWS V4 signature authentication
+Category: auth http
+Added: 7.75.0
+---
+Use AWS V4 signature authentication in the transfer.
+
+The provider argument is a string that is used by the algorithm when creating
+outgoing authentication headers.
